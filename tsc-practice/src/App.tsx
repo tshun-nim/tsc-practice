5d6924c85d6904fe27a9669be61ba4f633609649
tshun <s.t0811@icloud.com>
2024-07-04 02:13:55 +0900

    Todoアプリ UI作成中

--- tsc-practice/src/App.tsx
+++ tsc-practice/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	type Todo = {
+		inputValue: string;
+		id: number;
+		checked: boolean;
+	};
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		console.log(e.target.value);
+	};
+
+	return (
+		<div className="App">
+			<header>
+				<h2>Todoリスト</h2>
+				<form onSubmit={() => {}}>
+					<input type="text" onChange={(e) => handleChange(e)} />
+					<input type="button" value="作成" />
+				</form>
+			</header>
+		</div>
+	);
 }
 
 export default App;
