18be9c7691e95acb5df5d557a516c5c846b7039c
tshun <s.t0811@icloud.com>
2024-07-04 21:15:52 +0900

    Todo機能実装完了

--- tsc-practice/src/App.tsx
+++ tsc-practice/src/App.tsx
@@ -13,18 +13,76 @@ function App() {
 	};
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		console.log(e.target.value);
+		// console.log(e.target.value);
+		setInputValue(e.target.value);
 	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		// 新規Todo作成
+		const newTodo: Todo = {
+			inputValue: inputValue,
+			id: todos.length,
+			checked: false,
+		}
+
+		setTodos([newTodo, ...todos]);
+		setInputValue("");
+	};
+
+	const handleEdit = (id: number, inputValue: string) => {
+		const newTodos = todos.map((todo) => {
+			if(todo.id === id) {
+				todo.inputValue = inputValue;
+			}
+			return todo;
+		});
+
+		setTodos(newTodos);
+	}
+
+	const handleChecked = (id: number, checked: boolean) => {
+		const newTodos = todos.map((todo) => {
+			if(todo.id === id) {
+				todo.checked = !todo.checked;
+			}
+			return todo;
+		});
+		setTodos(newTodos);
+	}
+
+	const handleDelete = (id: number) => {
+		const newTodos = todos.filter((todo) => todo.id !== id) ;
+		setTodos(newTodos);
+	}
 
 	return (
 		<div className="App">
-			<header>
+			<div>
 				<h2>Todoリスト</h2>
-				<form onSubmit={() => {}}>
+				<form onSubmit={(e) => handleSubmit(e)} >
 					<input type="text" onChange={(e) => handleChange(e)} />
-					<input type="button" value="作成" />
+					<input type="submit" value="作成" />
 				</form>
-			</header>
+				<ul>
+					{todos.map((todo) => (
+						<li key={todo.id}>
+							<input
+								type="text"
+								onChange={(e) => handleEdit(todo.id, e.target.value)}
+								value={todo.inputValue}
+								disabled={todo.checked}
+							/>
+							<input
+								type="checkbox"
+								onChange={() => handleChecked(todo.id, todo.checked)}
+							/>
+							<button onClick={() => handleDelete(todo.id)}>削除</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
